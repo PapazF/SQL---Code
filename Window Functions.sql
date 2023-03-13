29e0ba2ed703a80ecda4bcfd68a6cd5b6b752643
Filip Papaz <filippapaz92@gmail.com>
2023-03-12 22:10:42 -0700

    Update Window Functions.sql

--- Window Functions.sql
+++ Window Functions.sql
@@ -1,8 +1,8 @@
 /* Employee salary vs average salary of his/her department */
 SELECT id, 
        last_name,
-	   department,
-	   salary,
+       department,
+       salary,
        AVG(salary) OVER (PARTITION BY department)
 FROM staff
 ORDER BY 3;
@@ -10,8 +10,8 @@ ORDER BY 3;
 /* Employee salary vs max salary of his/her department */
 SELECT id, 
        last_name,
-	   department,
-	   salary,
+       department,
+       salary,
        MAX(salary) OVER (PARTITION BY department)
 FROM staff
 ORDER BY 3;
@@ -19,8 +19,8 @@ ORDER BY 3;
 /* Employee salary vs min salary of his/her Company Region */
 SELECT id, 
        last_name,
-	   company_regions,
-	   salary,
+       company_regions,
+       salary,
        MIN(salary) OVER (PARTITION BY department)
 FROM staff AS s
 INNER JOIN company_regions AS c ON s.region_id = c.region_id
@@ -28,14 +28,14 @@ ORDER BY 3;
 
 /* FIRST_VALUE returns first value of the partition conditions, in this case decending order of salary group by department */
 SELECT department,
-	   salary,
+       salary,
        FIRST_VALUE(salary) OVER (PARTITION BY department ORDER BY salary DESC)
 FROM staff;
 
 /* Compare with the salary of person whose last_name is the first in ascenidng in that department */
 SELECT id,
        last_name,
-	   department,
+       department,
        salary,
        FIRST_VALUE(salary) OVER (PARTITION BY department ORDER BY last_name ASC)
 FROM staff;
@@ -44,8 +44,8 @@ FROM staff;
 The ranking 1, 2, 3 will restart when it reaches to another unique group.
 Works same as Row_Number Function */
 SELECT id,
-	   last_name,
-	   department,
+       last_name,
+       department,
        salary,
        RANK() OVER (PARTITION BY department ORDER BY salary DESC)
 FROM staff;
@@ -54,8 +54,8 @@ FROM staff;
 Also we can specify the number of rows from current row, by defining 'n', example LAG(column,n) */
 -- We want to know person's salary and next lower salary in that department
 SELECT id,
-	   last_name,
-	   department,
+       last_name,
+       department,
        salary,
        LAG(salary) OVER (PARTITION BY department ORDER BY salary ASC)
 FROM staff;
@@ -63,8 +63,8 @@ FROM staff;
 /* LEAD() function opposite of LAG() allows us to compare current row value with the values after the current row */
 -- We want to know person's salary and secound next higer salary in that department
 SELECT id,
-	   last_name,
-	   department,
+       last_name,
+       department,
        salary,
        LEAD(salary,2) OVER (PARTITION BY department ORDER BY salary ASC)
 FROM staff;
@@ -75,8 +75,8 @@ FROM staff;
 
 /* There are bins (1-10) Assigned each employees based on the decending salary of specific department, create bins in range (1-10) */
 SELECT id,
-	   last_name,
-	   department,
+       last_name,
+       department,
        salary,
        NTILE(10) OVER (PARTITION BY department ORDER BY salary DESC)
 FROM staff;
