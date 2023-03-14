5292d76f75133a710bf590fcef41dd36bb3bd90c
Filip Papaz <filippapaz92@gmail.com>
2023-03-14 09:43:10 -0700

    Update and rename Digital Music Store - Data Analysis Project.sql to Digital Music Store Project.sql

--- Digital Music Store Project.sql
+++ Digital Music Store Project.sql
@@ -1,25 +1,26 @@
 /***** SQL: Question Set 1 *****/
 
 /*Question 1: Which countries have the most Invoices?*/
-SELECT billing_country, COUNT(billing_country) AS invoice_number
+
+SELECT billing_country, 
+       COUNT(billing_country) AS invoice_number
 FROM invoice
 GROUP BY billing_country
 ORDER BY invoice_number DESC;
 
 /*Question 2: Which city has the best customers? 
-We would like to throw a promotional Music Festival in the city we made the most money. 
-Write a query that returns the 1 city that has the highest sum of invoice totals. 
-Return both the city name and the sum of all invoice totals.*/
-SELECT billing_city, SUM(total) AS invoice_total
+We would like to throw a promotional Music Festival in the city we made the most money. */
+
+SELECT billing_city, 
+       SUM(total) AS invoice_total
 FROM invoice
 GROUP BY billing_city
 ORDER BY invoice_total DESC
 LIMIT 1;
 
 /*Question 3: Who is the best customer?
-The customer who has spent the most money will be declared the best customer. 
-Build a query that returns the person who has spent the most money. 
-Invoice, InvoiceLine, and Customer tables to retrieve this information*/
+The customer who has spent the most money will be declared the best customer. */
+
 SELECT c.customer_id, c.first_name, c.last_name, SUM(i.total) AS total_spending
 FROM customer AS c
 JOIN invoice AS i ON c.customer_id = i.customer_id
@@ -30,6 +31,7 @@ LIMIT 1;
 /***** SQL: Question Set 2 *****/
 
 /*Question 1: Use your query to return the email, first name, last name, and genre of all Rock Music listeners. */
+
 SELECT DISTINCT email, first_name, last_name
 FROM customer
 JOIN invoice ON customer.customer_id = invoice.customer_id
@@ -44,8 +46,8 @@ ORDER BY email;
 
 /*Question 2: Who is the most popular rock artist?
 Now that we know that our customers love rock music, we can decide which musicians to invite to play at the concert.
-Let's invite the artists who have written the most rock music in our dataset. 
-Write a query that returns the Artist name and total track count of the top 10 rock bands.*/
+Let's invite the artists who have written the most rock music in our dataset. */
+
 SELECT artist.artist_id, artist.name, COUNT(artist.artist_id) AS number_of_songs
 FROM track
 JOIN album ON album.album_id = track.album_id
@@ -73,6 +75,7 @@ ORDER BY 1;
 
 
 /*Question 2: Return all the track names that have a song length longer than the average song length */
+
 SELECT name, miliseconds
 FROM track
 WHERE miliseconds > 
@@ -82,8 +85,7 @@ WHERE miliseconds >
 )
 ORDER BY miliseconds DESC;
 
-/*Question 3: Write a query that determines the customer that has spent the most on music for each country. 
-Write a query that returns the country along with the top customer and how much they spent. */
+/*Question 3: Write a query that determines the customer that has spent the most on music for each country. */
 SELECT billing_country, customer_id, full_name, MAX(total_spending)
 FROM 
 (
@@ -95,9 +97,3 @@ ORDER BY 1
 )
 GROUP BY 1,2
 ORDER BY 1;
-
-
-
-
-
-
